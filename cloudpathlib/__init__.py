6b02aa1408caa4445fd3f257fc331be42ae31a05
Paweł Wiejacha <pawel.wiejacha@rtbhouse.com>
2021-12-08 16:46:31 +0100

    GCS-only version

--- cloudpathlib/__init__.py
+++ cloudpathlib/__init__.py
@@ -1,13 +1,13 @@
 import sys
 
 from .anypath import AnyPath
-from .azure.azblobclient import AzureBlobClient
-from .azure.azblobpath import AzureBlobPath
+#from .azure.azblobclient import AzureBlobClient
+#from .azure.azblobpath import AzureBlobPath
 from .cloudpath import CloudPath, implementation_registry
-from .s3.s3client import S3Client
+#from .s3.s3client import S3Client
 from .gs.gspath import GSPath
 from .gs.gsclient import GSClient
-from .s3.s3path import S3Path
+#from .s3.s3path import S3Path
 
 
 if sys.version_info[:2] >= (3, 8):
@@ -21,12 +21,12 @@ __version__ = importlib_metadata.version(__name__.split(".", 1)[0])
 
 __all__ = [
     "AnyPath",
-    "AzureBlobClient",
-    "AzureBlobPath",
+#    "AzureBlobClient",
+#    "AzureBlobPath",
     "CloudPath",
     "implementation_registry",
     "GSClient",
     "GSPath",
-    "S3Client",
-    "S3Path",
+#    "S3Client",
+#    "S3Path",
 ]
